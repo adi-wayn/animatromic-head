23b907a17b22027b3d24310b08a6f5b29a1794b5
testmac <adiwayn@gmail.com>
2026-07-20 22:43:20 +0300

    Add jaw movement functionality and update eyelid angle limits

--- src/eyelid_right.h
+++ src/eyelid_right.h
@@ -7,8 +7,8 @@
 #define EYELID_RIGHT_CHANNEL 4
 
 // Safety limits for eyelid right
-#define EYELID_RIGHT_MIN_ANGLE 40.0
-#define EYELID_RIGHT_MAX_ANGLE 180.0
+#define EYELID_RIGHT_MIN_ANGLE 60.0  // Increased from 40 to prevent open-stall noise
+#define EYELID_RIGHT_MAX_ANGLE 140.0 // Decreased from 180 to prevent close-stall noise
 #define EYELID_RIGHT_CENTER_ANGLE 90.0
 
 // Eyelid right control task
