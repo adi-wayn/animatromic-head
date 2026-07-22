d34a78064eee7bef1d7f32e5ca668eb5513c7c2c
testmac <adiwayn@gmail.com>
2026-07-22 19:09:45 +0300

    feat(Phase 2): Refactor PoseController to strict primitive composition pattern

--- src/controllers/PoseController.cpp
+++ src/controllers/PoseController.cpp
@@ -20,6 +20,39 @@ void PoseController::executePose(const char* intent) {
     else resetToNeutral(); // Fallback
 }
 
+// --- Strict Atomic Building Blocks ---
+
+void PoseController::moveNeckPan(double angle, int durationMs, EasingType easing) {
+    KinematicEngine::getInstance().triggerMove(NECK_ONE, angle, durationMs, easing);
+}
+void PoseController::moveNeckTilt(double angle, int durationMs, EasingType easing) {
+    KinematicEngine::getInstance().triggerMove(NECK_Y, angle, durationMs, easing);
+}
+void PoseController::moveNeckRoll(double angle, int durationMs, EasingType easing) {
+    KinematicEngine::getInstance().triggerMove(NECK_ROLL, angle, durationMs, easing);
+}
+void PoseController::moveEyesPan(double angle, int durationMs, EasingType easing) {
+    KinematicEngine::getInstance().triggerMove(EYES_X, angle, durationMs, easing);
+}
+void PoseController::moveEyesTilt(double angle, int durationMs, EasingType easing) {
+    KinematicEngine::getInstance().triggerMove(EYES_Y, angle, durationMs, easing);
+}
+void PoseController::moveEyelidLeft(double angle, int durationMs, EasingType easing) {
+    KinematicEngine::getInstance().triggerMove(EYELID_LEFT, angle, durationMs, easing);
+}
+void PoseController::moveEyelidRight(double angle, int durationMs, EasingType easing) {
+    KinematicEngine::getInstance().triggerMove(EYELID_RIGHT, angle, durationMs, easing);
+}
+void PoseController::moveJawUD(double angle, int durationMs, EasingType easing) {
+    KinematicEngine::getInstance().triggerMove(JAW_UD, angle, durationMs, easing);
+}
+void PoseController::moveJawLR(double targetAngle, int durationMs, EasingType easing) {
+    // Hardware Defect Mitigation: strict clamping applied at the atomic level
+    if(targetAngle < JAW_LR.minAngle) targetAngle = JAW_LR.minAngle;
+    if(targetAngle > JAW_LR.maxAngle) targetAngle = JAW_LR.maxAngle;
+    KinematicEngine::getInstance().triggerMove(JAW_LR, targetAngle, durationMs, easing);
+}
+
 void PoseController::generateOrganicSaccade(uint32_t timeMs) {
     // Generate organic noise [-1.0, 1.0] using the existing wave superposition
     double noiseX = generateSimpleNoise(timeMs, 1.2f); 
@@ -31,38 +64,38 @@ void PoseController::generateOrganicSaccade(uint32_t timeMs) {
     double targetX = EYES_X.centerAngle + (noiseX * xRange);
     double targetY = EYES_Y.centerAngle + (noiseY * yRange);
 
-    KinematicEngine::getInstance().triggerMove(EYES_X, targetX, 100, EASE_OUT_EXPO);
-    KinematicEngine::getInstance().triggerMove(EYES_Y, targetY, 100, EASE_OUT_EXPO);
+    moveEyesPan(targetX, 100, EASE_OUT_EXPO);
+    moveEyesTilt(targetY, 100, EASE_OUT_EXPO);
 }
 
 // --- Basic Directions ---
 
 void PoseController::lookLeft(int durationMs, EasingType easing) {
-    KinematicEngine::getInstance().triggerMove(NECK_ONE, NECK_ONE.maxAngle, durationMs, easing);
-    KinematicEngine::getInstance().triggerMove(EYES_X, EYES_X.maxAngle, 150, EASE_OUT_EXPO);
+    moveNeckPan(NECK_ONE.maxAngle, durationMs, easing);
+    moveEyesPan(EYES_X.maxAngle, 150, EASE_OUT_EXPO);
 }
 
 void PoseController::lookRight(int durationMs, EasingType easing) {
-    KinematicEngine::getInstance().triggerMove(NECK_ONE, NECK_ONE.minAngle, durationMs, easing);
-    KinematicEngine::getInstance().triggerMove(EYES_X, EYES_X.minAngle, 150, EASE_OUT_EXPO);
+    moveNeckPan(NECK_ONE.minAngle, durationMs, easing);
+    moveEyesPan(EYES_X.minAngle, 150, EASE_OUT_EXPO);
 }
 
 void PoseController::lookUp(int durationMs, EasingType easing) {
-    KinematicEngine::getInstance().triggerMove(NECK_Y, NECK_Y.minAngle, durationMs, easing);
-    KinematicEngine::getInstance().triggerMove(EYES_Y, EYES_Y.minAngle, 150, EASE_OUT_EXPO); 
+    moveNeckTilt(NECK_Y.minAngle, durationMs, easing);
+    moveEyesTilt(EYES_Y.minAngle, 150, EASE_OUT_EXPO); 
 }
 
 void PoseController::lookDown(int durationMs, EasingType easing) {
-    KinematicEngine::getInstance().triggerMove(NECK_Y, NECK_Y.maxAngle, durationMs, easing);
-    KinematicEngine::getInstance().triggerMove(EYES_Y, EYES_Y.maxAngle, 150, EASE_OUT_EXPO);
+    moveNeckTilt(NECK_Y.maxAngle, durationMs, easing);
+    moveEyesTilt(EYES_Y.maxAngle, 150, EASE_OUT_EXPO);
 }
 
 void PoseController::tiltRight(int durationMs, EasingType easing) {
-    KinematicEngine::getInstance().triggerMove(NECK_ROLL, NECK_ROLL.maxAngle, durationMs, easing);
+    moveNeckRoll(NECK_ROLL.maxAngle, durationMs, easing);
 }
 
 void PoseController::tiltLeft(int durationMs, EasingType easing) {
-    KinematicEngine::getInstance().triggerMove(NECK_ROLL, NECK_ROLL.minAngle, durationMs, easing);
+    moveNeckRoll(NECK_ROLL.minAngle, durationMs, easing);
 }
 
 // --- Eyelids ---
@@ -76,30 +109,26 @@ void PoseController::blinkLeft(int durationMs, EasingType easing) {
 }
 
 void PoseController::blink(int durationMs, EasingType easing) {
-    KinematicEngine::getInstance().triggerMove(EYELID_LEFT, EYELID_LEFT.maxAngle, durationMs, easing);
-    KinematicEngine::getInstance().triggerMove(EYELID_RIGHT, EYELID_RIGHT.maxAngle, durationMs, easing);
+    moveEyelidLeft(EYELID_LEFT.maxAngle, durationMs, easing);
+    moveEyelidRight(EYELID_RIGHT.maxAngle, durationMs, easing);
 }
 
 // --- Jaw ---
 
 void PoseController::jawOpen(int durationMs, EasingType easing) {
-    KinematicEngine::getInstance().triggerMove(JAW_UD, JAW_UD.minAngle, durationMs, easing);
+    moveJawUD(JAW_UD.minAngle, durationMs, easing);
 }
 
 void PoseController::jawClose(int durationMs, EasingType easing) {
-    KinematicEngine::getInstance().triggerMove(JAW_UD, JAW_UD.maxAngle, durationMs, easing);
+    moveJawUD(JAW_UD.maxAngle, durationMs, easing);
 }
 
 void PoseController::jawLeft(int durationMs, EasingType easing) {
-    double target = JAW_LR.centerAngle - 10.0;
-    if(target < JAW_LR.minAngle) target = JAW_LR.minAngle;
-    KinematicEngine::getInstance().triggerMove(JAW_LR, target, durationMs, easing);
+    moveJawLR(JAW_LR.centerAngle - 10.0, durationMs, easing);
 }
 
 void PoseController::jawRight(int durationMs, EasingType easing) {
-    double target = JAW_LR.centerAngle + 10.0;
-    if(target > JAW_LR.maxAngle) target = JAW_LR.maxAngle;
-    KinematicEngine::getInstance().triggerMove(JAW_LR, target, durationMs, easing);
+    moveJawLR(JAW_LR.centerAngle + 10.0, durationMs, easing);
 }
 
 // --- Emotions / Reactions ---
@@ -113,33 +142,33 @@ void PoseController::expressHappy() {
 void PoseController::expressSad() {
     double halfClosedLeft = (EYELID_LEFT.minAngle + EYELID_LEFT.maxAngle) / 2;
     double halfClosedRight = (EYELID_RIGHT.minAngle + EYELID_RIGHT.maxAngle) / 2;
-    KinematicEngine::getInstance().triggerMove(EYELID_LEFT, halfClosedLeft, 250, EASE_OUT_EXPO);
-    KinematicEngine::getInstance().triggerMove(EYELID_RIGHT, halfClosedRight, 250, EASE_OUT_EXPO);
+    moveEyelidLeft(halfClosedLeft, 250, EASE_OUT_EXPO);
+    moveEyelidRight(halfClosedRight, 250, EASE_OUT_EXPO);
     
     jawClose(400, EASE_IN_OUT_SINE);
     lookDown(800, EASE_IN_OUT_CUBIC);
-    KinematicEngine::getInstance().triggerMove(NECK_ONE, NECK_ONE.centerAngle + 20, 800, EASE_IN_OUT_CUBIC);
+    moveNeckPan(NECK_ONE.centerAngle + 20, 800, EASE_IN_OUT_CUBIC);
 }
 
 void PoseController::expressThinking() {
-    KinematicEngine::getInstance().triggerMove(EYELID_LEFT, EYELID_LEFT.centerAngle, 200, EASE_OUT_EXPO);
-    KinematicEngine::getInstance().triggerMove(EYELID_RIGHT, EYELID_RIGHT.centerAngle, 200, EASE_OUT_EXPO);
+    moveEyelidLeft(EYELID_LEFT.centerAngle, 200, EASE_OUT_EXPO);
+    moveEyelidRight(EYELID_RIGHT.centerAngle, 200, EASE_OUT_EXPO);
     
     jawClose(400, EASE_IN_OUT_SINE);
-    KinematicEngine::getInstance().triggerMove(NECK_ONE, NECK_ONE.centerAngle + 15, 600, EASE_IN_OUT_CUBIC);
-    KinematicEngine::getInstance().triggerMove(NECK_Y, NECK_Y.centerAngle - 10, 600, EASE_IN_OUT_CUBIC);
+    moveNeckPan(NECK_ONE.centerAngle + 15, 600, EASE_IN_OUT_CUBIC);
+    moveNeckTilt(NECK_Y.centerAngle - 10, 600, EASE_IN_OUT_CUBIC);
 }
 
 void PoseController::resetToNeutral() {
-    KinematicEngine::getInstance().triggerMove(NECK_ONE, NECK_ONE.centerAngle, 600, EASE_IN_OUT_CUBIC);
-    KinematicEngine::getInstance().triggerMove(NECK_Y, NECK_Y.centerAngle, 600, EASE_IN_OUT_CUBIC);
-    KinematicEngine::getInstance().triggerMove(NECK_ROLL, NECK_ROLL.centerAngle, 600, EASE_IN_OUT_CUBIC);
+    moveNeckPan(NECK_ONE.centerAngle, 600, EASE_IN_OUT_CUBIC);
+    moveNeckTilt(NECK_Y.centerAngle, 600, EASE_IN_OUT_CUBIC);
+    moveNeckRoll(NECK_ROLL.centerAngle, 600, EASE_IN_OUT_CUBIC);
     
-    KinematicEngine::getInstance().triggerMove(EYES_X, EYES_X.centerAngle, 300, EASE_OUT_EXPO);
-    KinematicEngine::getInstance().triggerMove(EYES_Y, EYES_Y.centerAngle, 300, EASE_OUT_EXPO);
-    KinematicEngine::getInstance().triggerMove(EYELID_LEFT, EYELID_LEFT.minAngle, 300, EASE_OUT_EXPO);
-    KinematicEngine::getInstance().triggerMove(EYELID_RIGHT, EYELID_RIGHT.minAngle, 300, EASE_OUT_EXPO);
+    moveEyesPan(EYES_X.centerAngle, 300, EASE_OUT_EXPO);
+    moveEyesTilt(EYES_Y.centerAngle, 300, EASE_OUT_EXPO);
+    moveEyelidLeft(EYELID_LEFT.minAngle, 300, EASE_OUT_EXPO);
+    moveEyelidRight(EYELID_RIGHT.minAngle, 300, EASE_OUT_EXPO);
     
-    KinematicEngine::getInstance().triggerMove(JAW_UD, JAW_UD.centerAngle, 500, EASE_IN_OUT_SINE);
-    KinematicEngine::getInstance().triggerMove(JAW_LR, JAW_LR.centerAngle, 500, EASE_IN_OUT_SINE);
+    moveJawUD(JAW_UD.centerAngle, 500, EASE_IN_OUT_SINE);
+    moveJawLR(JAW_LR.centerAngle, 500, EASE_IN_OUT_SINE);
 }
